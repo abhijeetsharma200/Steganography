5f237ca5dae94df35dc6ff6a5f4f8f6357a5f30f
zafirazz <zafiraibraeva@gmail.com>
2024-02-29 18:58:43 +0100

    change: added comment to client

--- src/Client.java
+++ src/Client.java
@@ -8,7 +8,6 @@ import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
-//client
 public class Client {
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("localhost", 1234);
