8d7f10fd9ead72eb538b7c85570d8e9d9ef392fa
Abhijeet Sharma <abhijeetsharma2002@gmail.com>
2024-03-01 16:12:46 +0100

    Renamed Encrypt to Encoder

--- src/Encoder.java
+++ src/Encoder.java
@@ -3,7 +3,7 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
-public class Encrypt {
+public class Encoder {
     public static void main(String[] args) throws IOException{
         try {
             String catImgPath = "cat.png";
