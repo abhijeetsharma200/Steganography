083d9baf38adc06ab8f3950eff092ad50105b584
Abhijeet Sharma <abhijeetsharma2002@gmail.com>
2024-03-25 22:29:00 +0100

    Some fixes in Decoder and a lot of print statements

--- src/Decoder.java
+++ src/Decoder.java
@@ -15,25 +15,44 @@ public class Decoder {
             BufferedImage stgImg = ImageIO.read(new File(steganographicImgPath));
             BufferedImage decodedImage = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 
+            int counter = 0;
+
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     int stgImgRGB = stgImg.getRGB(x, y);
 
-                    int decodedRed = stgImgRGB & 0xF;
-                    int decodedGreen = (stgImgRGB >> 4) & 0xF;
-                    int decodedBlue = (stgImgRGB >> 8) & 0xF;
+                    String stgBinary = Integer.toBinaryString(stgImgRGB);
+                    String decodedString = stgBinary.substring(0, 8)+stgBinary.substring(12, 16)+"0010"+stgBinary.substring(20, 24)+"0100"+stgBinary.substring(28, 32)+"0100";
 
-                    decodedRed <<= 4;
-                    decodedGreen <<= 4;
-                    decodedBlue <<= 4;
+//                    int decodedRed = stgImgRGB & 0xF;
+//                    int decodedGreen = (stgImgRGB >> 4) & 0xF;
+//                    int decodedBlue = (stgImgRGB >> 8) & 0xF;
+//
+//                    decodedRed <<= 4;
+//                    decodedGreen <<= 4;
+//                    decodedBlue <<= 4;
+//
+//                    int decodedRGB = (255 << 24) | (decodedRed << 16) | (decodedGreen << 8) | decodedBlue;
 
-                    int decodedRGB = (255 << 24) | (decodedRed << 16) | (decodedGreen << 8) | decodedBlue;
+                    if(counter==1){
+                        System.out.println("stg RGB " + Integer.toBinaryString(stgImgRGB));
+//                        System.out.println(Integer.toBinaryString(stgImgRGB).length());
+                        System.out.println("decoded RGB " + decodedString);
+//                        System.out.println(decodedString.length());
+//                        System.out.println(decodedBlue);
+//                        System.out.println(decodedRed);
+//                        System.out.println(decodedGreen);
+                    }
 
-
-                    decodedImage.setRGB(x, y, decodedRGB);
+                    decodedImage.setRGB(x, y, Integer.parseUnsignedInt(decodedString, 2));
+                    int decodedRGB = decodedImage.getRGB(x, y);
+                    if(counter==1){
+                        System.out.println(Integer.toBinaryString(decodedRGB));
+                    }
+                    counter++;
                 }
             }
-
+            System.out.println(counter);
             ImageIO.write(decodedImage, "png", new File("decodedImage.png"));
 
         }
--- src/Encoder.java
+++ src/Encoder.java
@@ -16,31 +16,56 @@ public class Encoder {
 
             int catWidth = cat.getWidth();
             int catHeight = cat.getHeight();
-            int imageType = cat.getType();
-            System.out.println(imageType);
+//            int imageType = cat.getType();
             int dogWidth = dog.getWidth();
             int dogHeight = dog.getHeight();
-            System.out.println(dog.getType());
 
             BufferedImage modified_cat = new BufferedImage(catWidth, catHeight, BufferedImage.TYPE_3BYTE_BGR);
-            System.out.println(modified_cat.getType());
-            System.out.println(dogWidth);
-            System.out.println(dogHeight);
+
+            int count = 0;
 
             for(int y = 0; y < dogHeight; y++){
                 for(int x = 0; x < dogWidth; x++){
                     int catRGB = cat.getRGB(x,y);
                     int dogRGB = dog.getRGB(x, y);
 
+//                    String catBinary = Integer.toBinaryString(catRGB);
+//                    String dogBinary = Integer.toBinaryString(dogRGB);
+
+
+
                     int catRed = (catRGB >> 20) & 0xF;
                     int catGreen = (catRGB >> 12) & 0xF;
                     int catBlue = (catRGB >> 4) & 0xF;
+
+
 
                     int dogRed = (dogRGB >> 20) & 0xF;
                     int dogGreen = (dogRGB >> 12) & 0xF;
                     int dogBlue = (dogRGB >> 4) & 0xF;
 
+                    int encodedRed = (dogRGB >> 20) & 0xF;
+                    int encodedGreen = (dogRGB >> 12) & 0xF;
+                    int encodedBlue = (dogRGB >> 4) & 0xF;
+
                     int encodedRGB = (catRGB & 0xFFF0FFF0) | ((dogRGB & 0xF0F0F0F0) >>> 4);
+
+//                    if(count==1){
+//                        System.out.println("Cat RGB " + Integer.toBinaryString(catRGB));
+//                        System.out.println("Dog RGB " + Integer.toBinaryString(dogRGB));
+//                        System.out.println("Encoded RGB " + Integer.toBinaryString(encodedRGB));
+//                        System.out.println("Cat Red " + Integer.toBinaryString(catRed));
+////                        System.out.println("Cat Green " + Integer.toBinaryString(catGreen));
+////                        System.out.println("Cat Blue " + Integer.toBinaryString(catBlue));
+//                        System.out.println("Dog Red " + Integer.toBinaryString(dogRed));
+////                        System.out.println("Dog Green " + Integer.toBinaryString(dogGreen));
+////                        System.out.println("Dog Blue " + Integer.toBinaryString(dogBlue));
+////                        System.out.println("Original Red: " + Integer.toBinaryString(catRed) + " Original Green: " + Integer.toBinaryString(catGreen) + " Original Blue: " + Integer.toBinaryString(catBlue));
+////                        System.out.println("Encoded Red: " + Integer.toBinaryString(encodedRed) + " Encoded Green: " + Integer.toBinaryString(encodedGreen) + " Encoded Blue: " + Integer.toBinaryString(encodedBlue));
+//                    }
+//                    count++;
+
+//                    int encodedRGB = (catRGB & 0xFFF0FFF0) | ((dogRGB & 0xF0F0F0F0) >>> 4);
 
 
                     cat.setRGB(x, y, encodedRGB);
